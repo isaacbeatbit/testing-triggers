7c2810be57bef544c5f692e972fc2d987c2b3be6
isaac <beatbit01@gmail.com>
2022-04-14 14:48:22 -0500

    Initial commit

---
+++ test/testing-triggers.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestingTriggers from '../lib/testing-triggers-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/testing-triggers-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestingTriggers.TestingTriggersStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
